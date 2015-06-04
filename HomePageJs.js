624d026397561ac7ebf63c7dde20c2263c239057
songqy <songqy3@126.com>
2015-06-04 19:21:17 +0800

    5Dweb
    5Dweb

---
+++ HomePageJs.js
@@ -0,0 +1,21 @@
+
+function mOver1(obj){
+  $(obj).css({"margin-left":"20px", "margin-right":"40px","color":"#FF0000"});
+  $(obj).parent().css("text-shadow","-5px -5px 1px #0000FF");
+}
+
+function mOut1(obj){
+  $(obj).css({"margin-left":"30px","margin-right":"30px","color":"#FFFFFF"});
+  $(obj).parent().css("text-shadow","-5px -5px 10px #0000FF");
+}
+
+
+function mOver2(obj){
+  $(obj).css({"color":"#08EA08"});
+
+}
+
+function mOut2(obj){
+  $(obj).css({"color":"#008000"});
+
+}
